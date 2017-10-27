836c9df1e64f781aa9e1f9f79aa917bedffc1a2a
zongjian <cqzongjian@gmail.com>
2017-10-27 10:44:10 +0800

    update

--- angular4-reddit/src/app/app.component.ts
+++ angular4-reddit/src/app/app.component.ts
@@ -11,9 +11,12 @@ export class AppComponent {
   constructor() {
     this.articles = [
       new Article('MYSQL相关操作1', 'http://zongjian.site/single.html?post_id=103', 10),
-      new Article('CENTOS7部署SS-PANEL', 'http://zongjian.site/single.html?post_id=101', 5)
+      new Article('CENTOS7部署SS-PANEL', 'http://zongjian.site/single.html?post_id=101', 5),
+      new Article('TypeScript 入门指南', 'http://www.oschina.net/question/12_72250', 7)
     ];
   }
+
+  // 添加文章
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Add article title: ${title.value} and link: ${link.value}`);
     this.articles.push(new Article(title.value, link.value, 0));
@@ -21,4 +24,11 @@ export class AppComponent {
     link.value = '';
     return false;
   }
+
+  // 文章排序
+  sortedArtcles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+
+
 }
